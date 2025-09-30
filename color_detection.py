35d0a8450b9d6750c8523fb40fc3f63369956c2c
Joshua Pereira <joshua@cloud-adventures.net>
2025-09-30 10:38:15 -0400

    added aditional range for red hue

--- color_detection.py
+++ color_detection.py
@@ -12,10 +12,12 @@ while True:
     height = int(cap.get(4))
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([0, 180, 45]) 
-    upper_red = np.array([10, 255, 255])
+    lower_redA = np.array([0, 180, 45]) 
+    upper_redA = np.array([10, 255, 255])
+    lower_redB = np.array([170, 180, 45])
+    upper_redB = np.array([180, 255, 255])
 
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+    mask = cv2.inRange(hsv, lower_redA, upper_redA) | cv2.inRange(hsv, lower_redB, upper_redB)
 
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
